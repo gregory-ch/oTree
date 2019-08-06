bf77c0d0a0ed0e56c3b11b71589e756fb0b9829f
gregory-ch <chashire@ya.ru>
2019-08-06 07:35:40 +0300

    bot switcher false

--- settings.py
+++ settings.py
@@ -46,7 +46,7 @@ SESSION_CONFIGS = [
     'display_name': "Contracts_H_VS_R",
     'num_demo_participants': 4,
     'app_sequence': ['mpl','contracts_H_vs_R','my_survey'],
-    'use_browser_bots': True
+    'use_browser_bots': False
     },
 #
 # {
